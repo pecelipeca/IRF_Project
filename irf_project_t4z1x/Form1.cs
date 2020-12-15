91f27d7212036b23f8073c31eb28082285e99975
András István Péceli <andras.peceli@stud.uni-corvinus.hu>
2020-12-15 19:18:26 +0100

    restart

--- irf_project_t4z1x/Form1.cs
+++ irf_project_t4z1x/Form1.cs
@@ -17,37 +17,9 @@ namespace irf_project_t4z1x
         {
             InitializeComponent();
 
-            readText();
         }
 
-        void readText()
-        {
-            {
-                StreamReader reader = new StreamReader(File.OpenRead(@"C: \Users\pecel\source\Repos\IRF_Project\irf_project_t4z1x\project_test.csv"));
-                List<string> year = new List<String>();
-                List<string> month = new List<String>();
-                List<string> day = new List<String>();
-                List<string> value = new List<String>();
-                while (!reader.EndOfStream)
-                {
-                    string line = reader.ReadLine();
-                    if (!String.IsNullOrWhiteSpace(line))
-                    {
-                        string[] values = line.Split(';');
-                        if (values.Length >= 4)
-                        {
-                            year.Add(values[0]);
-                            month.Add(values[1]);
-                            day.Add(values[2]);
-                            value.Add(values[3]);
-                        }
-                    }
-                }
-                string[] firstlistA = year.ToArray();
-                string[] firstlistB = month.ToArray();
-                string[] firstlistC = day.ToArray();
-                string[] firstlistD = value.ToArray();
-            }
+
         }
     }
 }
